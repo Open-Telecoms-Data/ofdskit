a308459007896d02d8042fdc47a5494ed7364516
James <james.baster@opendataservices.coop>
2022-10-26 16:24:28 +0100

    geojsontojson: Start
    https://github.com/Open-Telecoms-Data/ofdskit/issues/1

--- ofdskit/cli/__main__.py
+++ ofdskit/cli/__main__.py
@@ -13,6 +13,11 @@ def main():
     json_to_geojson_parser.add_argument("outputnodesfilename")
     json_to_geojson_parser.add_argument("outputspansfilename")
 
+    geojson_to_json_parser = subparsers.add_parser("geojsontojson")
+    geojson_to_json_parser.add_argument("inputnodesfilename")
+    geojson_to_json_parser.add_argument("inputspansfilename")
+    geojson_to_json_parser.add_argument("outputfilename")
+
     args = parser.parse_args()
 
     if args.subparser_name == "jsontogeojson":
@@ -28,6 +33,20 @@ def main():
 
         with open(args.outputspansfilename, "w") as fp:
             json.dump(converter.get_spans_geojson(), fp, indent=4)
+
+    elif args.subparser_name == "geojsontojson":
+
+        with open(args.inputnodesfilename) as fp:
+            nodes_data = json.load(fp)
+
+        with open(args.inputspansfilename) as fp:
+            spans_data = json.load(fp)
+
+        converter = ofdskit.lib.geojson.GeoJSONToJSONConverter()
+        converter.process_data(nodes_data, spans_data)
+
+        with open(args.outputfilename, "w") as fp:
+            json.dump(converter.get_json(), fp, indent=4)
 
 
 if __name__ == "__main__":
---
+++ tests/test_geojson_to_json.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+import pytest
+
+from ofdskit.lib.geojson import GeoJSONToJSONConverter
+
+GEOJSON_TO_JSON_FILES = [
+    # basic example
+    ("basic_1"),
+]
+
+
+@pytest.mark.parametrize(
+    "filename",
+    GEOJSON_TO_JSON_FILES,
+)
+def test_geojson_to_json(filename):
+    nodes_filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "fixtures",
+        "geojson_to_json",
+        filename + ".nodes.geo.json",
+    )
+    spans_filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "fixtures",
+        "geojson_to_json",
+        filename + ".spans.geo.json",
+    )
+    expected_filename = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "fixtures",
+        "geojson_to_json",
+        filename + ".expected.json",
+    )
+
+    with open(nodes_filename) as fp:
+        nodes_data = json.load(fp)
+    with open(spans_filename) as fp:
+        span_data = json.load(fp)
+
+    converter = GeoJSONToJSONConverter()
+    converter.process_data(nodes_data, span_data)
+
+    with open(expected_filename) as fp:
+        expected_data = json.load(fp)
+    assert expected_data == converter.get_json()
