257984920e3340f215db2a88921bbe7975a14412
James <james.baster@opendataservices.coop>
2022-10-27 12:54:20 +0100

    release: 0.1.0

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import find_packages, setup
 
 setup(
     name="ofdskit",
-    version="0.0.0",
+    version="0.1.0",
     author="Open Data Services",
     author_email="code@opendataservices.coop",
     url="https://github.com/Open-Telecoms-Data/ofdskit",
+    project_urls={
+        "Documentation": "https://ofdskit.readthedocs.io/en/latest/",
+        "Issues": "https://github.com/Open-Telecoms-Data/ofdskit/issues",
+        "Source": "https://github.com/Open-Telecoms-Data/ofdskit",
+    },
     description="",
     license="MIT",
     packages=find_packages(),
